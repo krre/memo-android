443d6426942184ec53697938590ede952b2f542d
Vladimir Zarypov <krre31@gmail.com>
2024-03-04 07:38:18 +0200

    Fix module and theme names

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 
     QGuiApplication app(argc, argv);
 
-    QIcon::setThemeName("memo-qml");
+    QIcon::setThemeName("memo");
     QQuickStyle::setStyle("Material");
 
     auto memoTranslator = new QTranslator(&app);
